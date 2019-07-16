d545a7514036250d8fdc5c69dff7ddb081ed0e96
Kevin <brackkevin@gmail.com>
2019-07-16 16:22:51 -0500

    Removed comments

--- index.js
+++ index.js
@@ -24,8 +24,3 @@ module.exports = async (req, res) => {
   // Create response object and send back to slack
   res.end(JSON.stringify({ response_type, text: message, attachments }));
 };
-
-// module.exports = async (req, res) => {
-//   const body = await text(req);
-//   res.writeHead(200, { "Content-Type": "application/json" });
-// };
